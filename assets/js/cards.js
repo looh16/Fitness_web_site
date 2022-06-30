b1095d7cb7eac7d136db43f5ffe3dd1e735f67f2
Custodio Serafim Langa <langacustodio@gmail.com>
2022-06-30 19:08:04 +0200

    fix lint errors

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,40 +1,38 @@
-
-const container = document.getElementById("cardContent");
+const container = document.getElementById('cardContent');
 const parentDir = '../images/fitness/';
 
 const cards = [
-    {
-      img: `${parentDir}one.jpg`,
-      title: "Work hard in silence. Let success be your noise -Frank Ocean"
-    },
-    {
-      img: `${parentDir}two.jpg`,
-      title: "I try and groom myself, be it through fitness or dance. -Yami Gautam"
-    },
-    {
-      img: `${parentDir}three.jpg`,
-      title: "Fitness has nothing to do with age. -Virender Sehwag"
-    },
-    {
-      img: `${parentDir}four.jpg`,
-      title: "In fitness, there are no short cuts. It involves immense discipline and hard work. -Mahesh Babu"
-    },
-    {
-      img: `${parentDir}happy.jpg`,
-      title: "You can always improve your fitness if you keep training. -Pastor Maldonad"
-    },
-    {
-      img: `${parentDir}seven.jpg`,
-      title: "I understand only three things – films, fitness, and food. -Rakul Preet Singh"
-    }
-  ];
+  {
+    img: `${parentDir}one.jpg`,
+    title: 'Work hard in silence. Let success be your noise -Frank Ocean',
+  },
+  {
+    img: `${parentDir}two.jpg`,
+    title: 'I try and groom myself, be it through fitness or dance. -Yami Gautam',
+  },
+  {
+    img: `${parentDir}three.jpg`,
+    title: 'Fitness has nothing to do with age. -Virender Sehwag',
+  },
+  {
+    img: `${parentDir}four.jpg`,
+    title: 'In fitness, there are no short cuts. It involves immense discipline and hard work. -Mahesh Babu',
+  },
+  {
+    img: `${parentDir}happy.jpg`,
+    title: 'You can always improve your fitness if you keep training. -Pastor Maldonad',
+  },
+  {
+    img: `${parentDir}seven.jpg`,
+    title: 'I understand only three things – films, fitness, and food. -Rakul Preet Singh',
+  },
+];
 
-  
-  let htmlCard = '';
+let htmlCard = '';
 
 cards.forEach((card) => {
-  htmlCard += 
-  `
+  htmlCard
+  += `
   <div class="col-lg-4 col-md-6">
     <div class="activities-box shadow">
       <img src="${card.img}" alt="activities images" title="image 1" class="img-fluid">
@@ -49,5 +47,3 @@ cards.forEach((card) => {
 });
 
 container.innerHTML = htmlCard;
-
-
