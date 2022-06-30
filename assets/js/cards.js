41bdb9bc8c5581fc6b3eb3fc62f3684dd29b1424
Custodio Serafim Langa <langacustodio@gmail.com>
2022-06-30 10:22:19 +0200

    Add dynamic cards

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,41 +1,31 @@
-const container = document.getElementById("activitiesId");
-const parentDir = './images/';
+
+const container = document.getElementById("cardContent");
+const parentDir = '../images/fitness/';
 
 const cards = [
     {
-      img: "img/team/man.png",
-      name: "Bimal Timilsina",
-      position: "Web Designer",
-      facebook: "https://facebook.com/",
-      email: "mailto:someone@gmail.com"
+      img: `${parentDir}one.jpg`,
+      title: "Work hard in silence. Let success be your noise -Frank Ocean"
     },
     {
-      img: "img/team/man.png",
-      name: "Bimal Timilsina",
-      position: "Web Designer",
-      facebook: "https://facebook.com/",
-      email: "mailto:someone@gmail.com"
+      img: `${parentDir}two.jpg`,
+      title: "I try and groom myself, be it through fitness or dance. -Yami Gautam"
     },
     {
-      img: "img/team/man.png",
-      name: "Bimal Timilsina",
-      position: "Web Designer",
-       facebook: "https://facebook.com/",
-      email: "mailto:someone@gmail.com"
+      img: `${parentDir}three.jpg`,
+      title: "Fitness has nothing to do with age. -Virender Sehwag"
     },
     {
-      img: "img/team/man.png",
-      name: "Bimal Timilsina",
-      position: "Web Designer",
-       facebook: "https://facebook.com/",
-      email: "mailto:someone@gmail.com"
+      img: `${parentDir}four.jpg`,
+      title: "In fitness, there are no short cuts. It involves immense discipline and hard work. -Mahesh Babu"
     },
     {
-      img: "img/team/man.png",
-      name: "Bimal Timilsina",
-      position: "Web Designer",
-       facebook: "https://facebook.com/",
-      email: "mailto:someone@gmail.com"
+      img: `${parentDir}happy.jpg`,
+      title: "You can always improve your fitness if you keep training. -Pastor Maldonad"
+    },
+    {
+      img: `${parentDir}seven.jpg`,
+      title: "I understand only three things – films, fitness, and food. -Rakul Preet Singh"
     }
   ];
 
@@ -45,24 +35,19 @@ const cards = [
 cards.forEach((card) => {
   htmlCard += 
   `
-  <div class="row">
   <div class="col-lg-4 col-md-6">
     <div class="activities-box shadow">
-      <img src="images/fitness/happy.jpg" alt="activities images" title="image 1" class="img-fluid">
+      <img src="${card.img}" alt="activities images" title="image 1" class="img-fluid">
       <div class="activities-info">
         <div class="caption">
-          <h4>project name goes here 1</h4>
-          <p>category project</p>
+          <h5> ${card.title}</h5>
         </div>
       </div>
     </div>
   </div>
-</div>
-
 `;
 });
 
-const cardxx = document.querySelector('.cardContent');
-cardxx.innerHTML = htmlCard;
+container.innerHTML = htmlCard;
 
 
